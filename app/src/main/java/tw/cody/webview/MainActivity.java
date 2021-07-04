e23eb7b19a82cc27a6af10c4425fa6ede4e748d2
cody30605 <qwer30605302186@gmail.com>
2021-07-04 18:36:52 +0800

    Initial commit

---
+++ app/src/main/java/tw/cody/webview/MainActivity.java
@@ -0,0 +1,70 @@
+package tw.cody.webview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.LocaleList;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+
+
+//        webView.setWebViewClient(new webClient());  //1
+//        WebChromeClient chromeClient = new WebChromeClient();
+//        webView.setWebChromeClient(chromeClient);
+
+        WebSettings settings = webView.getSettings();
+//        webView.loadUrl("file:///android_asset/brad.html");
+        webView.loadUrl("https://github.com/cody30605?tab=repositories");
+        settings.setJavaScriptEnabled(true);
+        WebViewClient client = new WebViewClient();
+        webView.setWebViewClient(client);
+
+    }
+
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
+}
